4d64439cda526b22522e39c19448466ad5f609da
Gyanpriya <gyanpriya37@gmail.com>
2025-09-04 16:45:58 +0530

    version 1

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+# Load pipeline (preprocessor + model)
+model = pickle.load(open(r"C:\Users\hp\Documents\class\pipe.pkl", "rb"))
+
+st.title("🏠 House Price Prediction")
+
+# Take input from user
+location = st.text_input("Enter location: ")
+area_type = st.text_input("Enter area type: ")
+total_sqft = st.number_input("Enter total square feet: ", min_value=0.0)
+bath = st.number_input("Enter number of bathrooms: ", min_value=0)
+bhk = st.number_input("Enter number of bedrooms (BHK): ", min_value=0)
+
+# Create DataFrame
+user_input = pd.DataFrame([[location, area_type, total_sqft, bath, bhk]],
+                          columns=['location', 'area_type', 'total_sqft', 'bath', 'bhk'])
+
+if st.button("Predict Price"):
+    predicted_price = model.predict(user_input)
+    st.success(f"The predicted price is: {predicted_price[0]:.2f} Lakhs")
